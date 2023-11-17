7f0bb26c2d38c7826f7a1fca2668c37ec19321d4
CodeforEvolution <secundaja@gmail.com>
2023-11-17 10:52:26 -0600

    Some WIP Changes
    Change-Id: I31b5628069ecdbfea9c3ff4823165640278ffaf6

--- src/preferences/bluetooth/defs.h
+++ src/preferences/bluetooth/defs.h
@@ -12,22 +12,18 @@
 #define TRY_SETTINGS 'trys'
 
 #define ATTRIBUTE_CHOSEN 'atch'
-#define UPDATE_COLOR 'upcl'
-#define DECORATOR_CHOSEN 'dcch'
-#define UPDATE_DECORATOR 'updc'
-#define UPDATE_COLOR_SET 'upcs'
 
 #define SET_VISIBLE 		'sVis'
 #define SET_DISCOVERABLE 	'sDis'
 #define SET_AUTHENTICATION 	'sAth'
 
-#define SET_UI_COLORS 'suic'
 #define PREFS_CHOSEN 'prch'
 
 // user interface
 const uint32 kBorderSpace = 10;
 const uint32 kItemSpace = 7;
 
+// message types
 static const uint32 kMsgAddToRemoteList = 'aDdL';
 static const uint32 kMsgRefresh = 'rFLd';
 
@@ -36,6 +32,10 @@ static const int32 kMsgSetDeviceClass = 'sDC0';
 static const int32 kMsgSetInquiryTime = 'afEa';
 static const int32 kMsgLocalSwitched = 'lDsW';
 
+static const uint32 kMsgDeviceListSelection = 'devs';
+static const uint32 kMsgDeviceListInvoke = 'devi';
+
+// globals
 extern LocalDevice* gActiveLocalDevice;
 
 #endif
